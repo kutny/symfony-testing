bbf4a3bc0f29ee06043c9857d419fd978b89f202
Honza Machala <koutny@collabim.com>
2012-11-16 15:06:07 +0100

    Form testing controller introduced

---
+++ src/Acme/HelloBundle/Controller/FormTestController.php
@@ -0,0 +1,41 @@
+<?php
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class FormTestController extends Controller
+{
+	public function indexAction(Request $request)
+	{
+		$form = $this->prepareForm();
+
+		if ($request->getMethod() === 'POST') {
+			$this->saveForm($form, $request);
+		}
+
+		return $this->render('AcmeHelloBundle:FormTest:index.html.twig', array(
+			'form' => $form->createView()
+		));
+	}
+
+	private function prepareForm()
+	{
+		return $this->createForm(new \Acme\HelloBundle\Form\TestPostForm(), array(
+			'note' => 'some default note'
+		));
+	}
+
+	private function saveForm(\Symfony\Component\Form\Form $form, Request $request)
+	{
+		$form->bind($request);
+
+		if ($form->isValid()) {
+			$this->get('session')->setFlash('notice', 'Your changes were saved!');
+
+			$this->redirect($this->generateUrl('route.formTest'));
+		}
+	}
+
+}
---
+++ src/Acme/HelloBundle/Form/TestPostForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Acme\HelloBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class TestPostForm extends AbstractType
+{
+	public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
+	{
+		$builder->add('note', 'text', array(
+			'constraints' => array(
+				new \Symfony\Component\Validator\Constraints\MaxLength(3)
+			),
+			'label' => 'Some custom label' // NOTE: we should NOT set label here for the HTML coder/designer to be able to change it
+		));
+
+		$builder->add('dueDate', 'date');
+
+		$builder->add('trueFalse', new \Symfony\Component\Form\Extension\Core\Type\CheckboxType());
+
+		$builder->add('select', new \Symfony\Component\Form\Extension\Core\Type\ChoiceType(), array(
+			'choices' => array(
+				1 => 'Jirka'
+			)
+		));
+
+		$builder->add('email', new \Symfony\Component\Form\Extension\Core\Type\EmailType());
+	}
+
+	public function getName()
+	{
+		return 'testPost';
+	}
+}
